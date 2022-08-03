717e3b30e9a9c4ea0ed2e5fed41b97b63189a3a1
Nino Lindenberg <93713481+Aeternalis-Ingenium@users.noreply.github.com>
2022-08-03 09:39:53 +0200

    fix: Update AsyncSession

--- backend/app/database/repositories/base.py
+++ backend/app/database/repositories/base.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 class BaseRepository:
     def __init__(self, session: AsyncSession) -> None:
-        self._session = session
+        self._session: AsyncSession = session
 
     @property
     def session(self) -> AsyncSession:
